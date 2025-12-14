c704b118caaf3ceabd6f5c59bd459cd74ed56383
Amith Adiraju <amithadiraju@Amiths-Laptop.local>
2025-12-14 10:44:01 -0800

    Added helpers to created oxford semantic segmentation data set. Modified helpers to split semantic seg without stratified split.
    Signed-off-by: Amith Adiraju <amithadiraju@Amiths-Laptop.local>

--- helpers.py
+++ helpers.py
@@ -1,10 +1,62 @@
 import torch
-from torch.utils.data import DataLoader, TensorDataset
+from torch.utils.data import DataLoader, TensorDataset, Dataset
 from torchvision.transforms import v2 as transforms
 from torchvision import datasets
 from typing import Optional, Tuple, Union
 import os
 from helpers_profiling import *
+
+IMAGENET_MEAN = (0.485, 0.456, 0.406); IMAGENET_STD  = (0.229, 0.224, 0.225)
+
+class OxfordPetSeg(Dataset):
+    def __init__(self, data_dir, split="train"):
+        base_split = "trainval" if split != "test" else "test"
+        self.base = datasets.OxfordIIITPet(
+            root=data_dir,
+            split=base_split,
+            target_types="segmentation",
+            download=True,
+            transform=None,
+            target_transform=None,
+        )
+
+        n = len(self.base)
+        self.indices = range(0, n)
+        if split == "train":
+             # read full train and val combined data set and split later
+            self.common_tf = transforms.Compose([
+                transforms.ToImage(),
+                transforms.RandomResizedCrop(224, scale=(0.5, 1.0)),
+                transforms.RandomHorizontalFlip(),
+            ])
+        else:
+            # read full test data set and split later
+            self.common_tf = transforms.Compose([
+                transforms.ToImage(),
+                transforms.Resize((224, 224)),
+            ])
+
+        self.img_tf = transforms.Compose([
+            transforms.ToDtype(torch.float32, scale=True),
+            transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+        ])
+
+    def __len__(self):
+        return len(self.indices)
+
+    def __getitem__(self, idx):
+        img, mask = self.base[self.indices[idx]]
+        
+        # if trainval - crop/flip/resize; if test - toimage, resize
+        img, mask = self.common_tf(img, mask)
+
+        # Same image transforms to trian and test images
+        img = self.img_tf(img)
+        
+        # Same mask transforms to train and test images
+        mask = mask.squeeze(0).to(torch.long)
+        
+        return img, mask
 
 
 def entropy_vec(x: torch.Tensor, req_dim: int = 2, scale_entropy = True) -> torch.Tensor:
@@ -485,31 +537,11 @@ def get_cifar100_loaders_optimized(data_dir: str='./data') -> Tuple[torch.utils.
 
 def get_oxfordseg_loaders(data_dir = './data'):
 
-    IMAGENET_MEAN = (0.485, 0.456, 0.406); IMAGENET_STD  = (0.229, 0.224, 0.225)
+    train_dataset = OxfordPetSeg(data_dir=data_dir, split="train")
+    test_dataset = OxfordPetSeg(data_dir=data_dir, split="test")
 
-    common_train_transform = transforms.Compose([
-            transforms.ToImage(),  # PIL -> (C,H,W) uint8
-            transforms.RandomResizedCrop(224, scale=(0.5, 1.0)),
-            transforms.RandomHorizontalFlip(),
-        ])
+    return train_dataset, test_dataset
     
-    test_transform = transforms.Compose([
-                            transforms.ToImage(),
-                            transforms.Resize((224, 224)),
-                                        ])
-    
-    train_img_transform = transforms.Compose([
-            transforms.ToDtype(torch.float32, scale=True),
-            transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
-        ])
-
-    datasets.OxfordIIITPet(
-        root = data_dir, split = "trainval", target_types="segmentation",
-        download = True)
-    
-
-
-
 def save_cached_split(ds, path: str, batch_size: int=512, num_workers: int=8, dtype=torch.float16) -> None:
 
     """Saves tensor data in specified path with the raw torch data set provided."""
@@ -529,13 +561,15 @@ def save_cached_split(ds, path: str, batch_size: int=512, num_workers: int=8, dt
     X = torch.cat(Xs); Y = torch.cat(Ys)
     torch.save({"images": X, "labels": Y}, path)
 
+
 def split_pt_file_stratified(src_path,
                              split1_path="validation_ablations.pt",
                              split2_path="test_ablations.pt",
                              split1_frac=0.7,
                              seed=108,
+                             split_stratified = True,
                              save_both_splits = True
-                             ):
+                             ) -> None:
     
     """ Splits a single tensor file into two tensor files and writes them to disk as tensors.
     It assumes that single tensor file contains features and labels with anmes "images", "labels".
@@ -544,22 +578,37 @@ def split_pt_file_stratified(src_path,
     """
 
     d = torch.load(src_path, map_location= "cpu")
-
+    # CIFAR-X , Oxford supports this
     X, Y = d["images"], d["labels"]
     assert X.shape[0] == Y.shape[0], "Mismatched X/Y lengths"
     g = torch.Generator().manual_seed(seed)
 
     split1_idx, split2_idx = [], []
-    # Stratified, balanced sampling
-    for c in torch.unique(Y).tolist():
-        
-        idx = torch.where(Y == c)[0]
-        perm = idx[torch.randperm(idx.numel(), generator=g)]
 
-        n_val = max(1, min(idx.numel() - 1, int(idx.numel() * split1_frac)))
+    # Assumes labels are multi-class
+    if split_stratified:
+
+        # Stratified, balanced sampling
+        for c in torch.unique(Y).tolist():
+            
+            idx = torch.where(Y == c)[0]
+            perm = idx[torch.randperm(idx.numel(), generator=g)]
+
+            n_val = max(1, min(idx.numel() - 1, int(idx.numel() * split1_frac)))
+            
+            split1_idx.append(perm[:n_val])
+            split2_idx.append(perm[n_val:])
+    
+    # Labels are not assumed
+    # standard random split of X, Y
+    else:
+        total_indices = torch.arange(X.shape[0])
+        perm = total_indices[torch.randperm(total_indices.numel(), generator=g)]
+        n_split1 = int(len(perm) * split1_frac)
         
-        split1_idx.append(perm[:n_val])
-        split2_idx.append(perm[n_val:])
+        # Though lists are not required for splits. keeping them to avoid multiple downstream changes.
+        split1_idx.append( perm[:n_split1] )
+        split2_idx.append( perm[n_split1:] )
 
     split1_idx = torch.cat(split1_idx)
     split2_idx = torch.cat(split2_idx)
@@ -568,6 +617,7 @@ def split_pt_file_stratified(src_path,
     split1_idx = split1_idx[torch.randperm(split1_idx.numel(), generator=g)]
     split2_idx = split2_idx[torch.randperm(split2_idx.numel(), generator=g)]
 
+    # CIFAR-X , Oxford supports this
     split1 = {"images": X[split1_idx].contiguous(), "labels": Y[split1_idx].contiguous()}
     split1_dataset = TensorDataset(split1["images"], split1["labels"])
     
@@ -581,14 +631,14 @@ def split_pt_file_stratified(src_path,
     
 
     if save_both_splits:
+        # CIFAR-X , Oxford supports this
         split2 = {"images": X[split2_idx].contiguous(), "labels": Y[split2_idx].contiguous()}
         split2_dataset = TensorDataset(split2["images"], split2["labels"])
         
         save_cached_split(split2_dataset,   split2_path, dtype = device_dtype)
         print("Written Split 2 dataset ")
 
-
-def make_cached_loader(path: str, batch_size: int=512, shuffle: bool =True, num_workers:int =8):
+def make_cached_loader(path: str, batch_size: int=512, shuffle: bool =True, num_workers:int =8) -> DataLoader:
 
     """Function that loads pre-cached tensor data from specified path and returns a dataloader."""
 
@@ -596,7 +646,7 @@ def make_cached_loader(path: str, batch_size: int=512, shuffle: bool =True, num_
         raise ValueError(f"Cached data not found in path: {path}. Please run prepare_cached_datasets function first.")
     
     blob = torch.load(path, map_location="cpu")
-    
+    # CIFAR-X, Oxford supports this
     ds = TensorDataset(blob["images"], blob["labels"])
     
     pin_memory = False; persistent_workers = False
@@ -611,8 +661,9 @@ def make_cached_loader(path: str, batch_size: int=512, shuffle: bool =True, num_
                       pin_memory=pin_memory,
                       persistent_workers=persistent_workers)
 
+
 def prepare_cached_datasets(cached_data_path: str, dataset = "cifar10") -> dict:
-    """ Function that prepares tensors from cifar10 data set , with specified transforms and stores them in current project space.
+    """ Function that prepares tensors from data set, splits into train and val and stores them in current project space.
     Uses torch.float16 if cuda is available, else torch.float32 on saved tensors.
     Cached path should end with / .
 
@@ -622,6 +673,7 @@ def prepare_cached_datasets(cached_data_path: str, dataset = "cifar10") -> dict:
         data_paths: Dictionary containing paths to train, val and test tensor data.
     """
     
+    # Check if pre-computed tensors already exist in specified folder.
     if os.path.exists(cached_data_path + "train.pt") and os.path.exists(cached_data_path + "val.pt") and os.path.exists(cached_data_path + "test.pt"):
         print("Cached data already exists. Skipping caching step.")
         data_paths = {'train_data': cached_data_path + "train.pt", 'val_data': cached_data_path + "val.pt", 'test_data': cached_data_path + "test.pt"}
@@ -632,7 +684,9 @@ def prepare_cached_datasets(cached_data_path: str, dataset = "cifar10") -> dict:
         train_raw , test_raw = get_cifar10_loaders_optimized(data_dir='./data')
     elif dataset == "cifar100":
         train_raw , test_raw = get_cifar100_loaders_optimized(data_dir='./data')
-    
+    elif dataset == "semseg_oxford":
+        train_raw, test_raw  = get_oxfordseg_loaders(data_dir='./data')
+
     idx = torch.randperm(len(train_raw))
     cut = int(0.7 * len(train_raw))
 
